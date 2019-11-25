6937565a34971406a368b561212b7b31d61059f5
RohithGadekari <190030458@kluniversity.in>
2019-11-25 09:44:35 +0530

    created my first c program

---
+++ area and perimeter.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+	int r,a,b;
+	float pie,ar1,per1,ar2,per2;
+	printf("enter radius");
+	scanf("%d",&r);
+	printf("enter values of a,b");
+	scanf("%d%d",&a,&b);
+	pie=3.14;
+	ar1=pie*r*r;
+	per1=2*pie*r*r;
+	printf("area of circle is %d",ar1);
+	printf("perimeter of circle is %d",per1);
+	ar2=a*b;
+	per2=2*(a+b);
+	printf("area of rectangle is %d ",ar2);
+	printf("perimeter of rectangle is %d",per2);
+	return 0;
+}
